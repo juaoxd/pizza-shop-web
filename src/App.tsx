a5c7fb5257f0824337cfb23318c3714d7707f9a0
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-04-11 01:12:11 -0300

    chore: setup eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './index.css'
 
-
 import { Button } from './components/ui/button'
 
 export function App() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className="flex h-screen items-center justify-center">
       <Button>Click me</Button>
     </div>
   )
