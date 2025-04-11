4e2c0f4a44e1f706851c1f04502cd848846bc62b
João Gabriel <joaogabrielbochlivio@gmail.com>
2025-04-11 00:53:13 -0300

    feat: add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import './index.css'
+
+
+import { Button } from './components/ui/button'
+
 export function App() {
   return (
-    <h1>hello world</h1>
+    <div className='flex justify-center items-center h-screen'>
+      <Button>Click me</Button>
+    </div>
   )
 }
